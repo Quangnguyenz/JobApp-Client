469af91ec3bf7f35b4002912ef2cf9ad4502b7f5
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2024-01-29 00:21:36 -0800

    node

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h1>HomeLayout</h1>;
+  return (
+    <div>
+      <h1>HomeLayout</h1>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 export default HomeLayout;
