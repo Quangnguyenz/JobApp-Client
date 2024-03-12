88e8ef6177ccc63ac8843c54b184dd9e4701b047
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2024-03-11 23:58:04 -0700

    Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 const StyledBtn = styled.button`
   font-size: 1.5rem;
@@ -8,10 +8,22 @@ const StyledBtn = styled.button`
 
 const Landing = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>Landing Page</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+      <div className="content">some content</div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
+
 export default Landing;
